203f0f204db0a52f29ef90f612acf8e6079346a5
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-04-21 14:56:12 -0400

    Added a HashMap, and input for Q&A

--- src/flashcardapp/FlashcardApp.java
+++ src/flashcardapp/FlashcardApp.java
@@ -1,19 +1,52 @@
 package flashcardapp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class FlashcardApp {
 
     public static void main(String[] args) {
         
-        ArrayList<Flashcard> cards = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
         
-        cards.add(new Flashcard("Can i bark", "no"));
-        cards.add(new Flashcard("Is the sky blue" , "no"));
+        ArrayList<Flashcard> cards = new ArrayList<>();
+        HashMap<String, Flashcard> questionNumber = new HashMap<>();
+        
+        boolean stay = true;
+        
+        System.out.println("""
+                               Welcome to your Study Buddy!
+                               Press 1 to add your first question & answer!   
+                               """);
+        
+        while(stay){
+           
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch (option) {
+                case 1 -> {
+                    System.out.println("What's your question?");
+                    String question = scanner.nextLine();
+                    System.out.println("Now what's the answer?");
+                    String answer = scanner.nextLine();
+                    
+                    cards.add(new Flashcard(question, answer));
+                    System.out.println("Here is your card: " + cards.get(0));
+                }
+            }
+        }
         
         for(int i = 0; i < cards.size(); i++){
+            // Assigns i + 1 into a String value, basically when i is 1, 
+            // it'll be "1" after. 
+            questionNumber.put(String.valueOf(i + 1), cards.get(i));
             System.out.println(i + 1  + ". " + cards.get(i));
         }
     }
    
+    
+    
+    
 }
