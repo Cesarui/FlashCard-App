1e20211afbdd5500ff87343f8310060d781a356f
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-05-12 21:57:53 -0400

    Getters and Setters

--- src/flashcardapp/Flashcard.java
+++ src/flashcardapp/Flashcard.java
@@ -11,6 +11,26 @@ public class Flashcard {
         this.answer = answer;
     }
     
+    public String getAnswer(){
+        
+        return answer;
+    }
+    
+    public void setAnswer(String answer){
+        
+        this.answer = answer;
+    }
+    
+    public String getQuestion(){
+        
+        return question;
+    }
+    
+    public void setQuestion(String question) {
+        
+        this.question = question;
+    }
+    
     @Override
     public String toString(){
         return "Q: " + question + " | A: " + answer;
